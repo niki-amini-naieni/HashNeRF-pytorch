e8318d3a75698c35f516601db30d8f9475baf0ae
Niki Amini-Naieni <niki@adainnovations.ai>
2023-11-08 15:05:51 +0000

    took order out of print func input

--- derivative_check.py
+++ derivative_check.py
@@ -1,10 +1,11 @@
-from sympy import *
+from autodiff import function, gradient
 import numpy as np
 
 betas = np.random.random(128)
 mus = np.random.random(128)
 pis = np.random.random(128)
 
+@gradient
 def cdf(x, mus, betas, pis):
     return np.sum(pis * (0.5 + 0.5 * np.sign(x - mus) * (1 - np.exp(-np.abs(x - mus) / betas))))
 
@@ -13,7 +14,8 @@ def pdf(x, mus, betas, pis):
     return np.sum(pis * (1 / (2 * betas)) * np.exp(-np.abs(x - mus) / betas))
 
 
-x = Symbol('x')
-y = cdf(x, mus, betas, pis)
-yprime = y.diff(x)
-print(y_prime)
+x = 0
+y_autodiff = cdf(x, mus, betas, pis)
+y = pdf(x, mus, betas, pis)
+print(y_autodiff)
+print(y)
