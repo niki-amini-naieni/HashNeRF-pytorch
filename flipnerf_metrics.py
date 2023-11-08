a6831f4b444bca0ae4363e6f301a24d77f753dcb
Niki Amini-Naieni <niki@adainnovations.ai>
2023-11-08 05:14:30 +0000

    revised name of local get_uncert

--- flipnerf_metrics.py
+++ flipnerf_metrics.py
@@ -103,13 +103,13 @@ def adjust_for_quantile(ps, cs):
 
 def get_uncerts(mus, betas, pis, A_R, A_G, A_B, cal, num_procs=12):
     
-    global get_uncerts
+    global get_uncert
     num_mix_comps = mus.shape[-2]
     mus = mus.reshape(-1, num_mix_comps, 3)
     betas = betas.reshape(-1, num_mix_comps, 3)
     pis = pis.reshape(-1, num_mix_comps)
     uncerts = mp.Array("d", np.zeros(pis.shape[0]), lock=False)
-    def get_uncerts(px_ind):
+    def get_uncert(px_ind):
         mu = mus[px_ind]
         beta = betas[px_ind]
         pi = pis[px_ind]
@@ -146,7 +146,7 @@ def get_uncerts(mus, betas, pis, A_R, A_G, A_B, cal, num_procs=12):
         uncerts[px_ind] = (interquart_r + interquart_g + interquart_b) / 3
 
     proc_pool = mp.Pool(num_procs)
-    proc_pool.map(get_uncerts, range(pis.shape[0]))
+    proc_pool.map(get_uncert, range(pis.shape[0]))
     proc_pool.close()
     proc_pool.join()
 
